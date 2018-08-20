9306f6f4c1ed5e36b7cdc0fe8934d60a4be07de9
wanjikum <wanjikumillicent5@gmail.com>
2018-08-20 20:04:42 +0300

    fix: create database

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const product = require('./routes/product.route'); // Imports routes for the products
+
 // initialize our express app
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://Millicent:millicent1@ds125472.mlab.com:25472/millyproductstutorial';
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use('/products', product);
 
 let port = 1234;
 
@@ -10,3 +23,4 @@ let port = 1234;
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
 });
+
